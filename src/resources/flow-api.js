5f1aaf301dc902fc17e31d04c7ad6344fa9fa7ca
Alejandro Brozzo <alebrozzo@msn.com>
2018-07-13 21:11:29 -0300

    Added lag time to simulate API hit

--- src/resources/flow-api.js
+++ src/resources/flow-api.js
@@ -1,56 +1,59 @@
 export const getFlowFunctions = () =>
+    // simulate an API hit delay of a random time between 0.2 and 1 seconds
     new Promise((resolve, reject) => {
-        resolve([
-            {
-                id: 1,
-                title: "Verify if weekend",
-                body: `function isWeekend(date) {
+        setTimeout(() => resolve(getData()), Math.floor(Math.random() * 800) + 200);
+    });
+
+export const getData = () => [
+    {
+        id: 1,
+        title: "Verify if weekend",
+        body: `function isWeekend(date) {
   const dayOfWeek = date.getDay();
   return (dayOfWeek === 6) || (dayOfWeek === 0);
 }`,
-                true_id: 2,
-                false_id: 3,
-            },
-            {
-                id: 2,
-                title: "Celebrate",
-                body: `function celebrate() {
+        true_id: 2,
+        false_id: 3,
+    },
+    {
+        id: 2,
+        title: "Celebrate",
+        body: `function celebrate() {
   console.log('Party on!!!');
   return true;
 }`,
-                true_id: null,
-                false_id: null,
-            },
-            {
-                id: 3,
-                title: "Verify if Friday",
-                body: `function isItFriday(date) {
+        true_id: null,
+        false_id: null,
+    },
+    {
+        id: 3,
+        title: "Verify if Friday",
+        body: `function isItFriday(date) {
   const dayOfWeek = date.getDay();
   return dayOfWeek === 5;
 }`,
-                true_id: 4,
-                false_id: 5,
-            },
-            {
-                id: 4,
-                title: "TGIF",
-                body: `function tgif() {
+        true_id: 4,
+        false_id: 5,
+    },
+    {
+        id: 4,
+        title: "TGIF",
+        body: `function tgif() {
   console.log('TGIF!');
   return true;
 }`,
-                true_id: null,
-                false_id: null,
-            },
-            {
-                id: 5,
-                title: "Groan",
-                body: `function groan(date) {
+        true_id: null,
+        false_id: null,
+    },
+    {
+        id: 5,
+        title: "Groan",
+        body: `function groan(date) {
   const daysUntilWeekend = 6 - date.getDay();
   console.log('Hang in there... ' + daysUntilWeekend + ' days until the weekend!');
   return true;
 }`,
-                true_id: null,
-                false_id: null,
-            },
-        ]);
-    });
+        true_id: null,
+        false_id: null,
+    },
+];
